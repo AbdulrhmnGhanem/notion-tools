e300c8b9c8b8498bd01fca010781f87a64483a20
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-12-30 21:00:47 +0200

    Feat: Read the notion token and the DB ID from keyring

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    pass
+from typing import Final
+import keyring
+
+
+def read_auth():
+    """Read the notion access token and the Reading list notion DB from keyring."""
+    access_token: Final = keyring.get_password("notion", "weekly-articles-selector")
+    reading_list_id: Final = keyring.get_password("notion", "reading-list")
+
+    if access_token == None:
+        raise ValueError("Access token not found!")
+
+    if reading_list_id == None:
+        raise ValueError('"Reading List" database id not found!')
+
+    return access_token, reading_list_id
+
+
+def main(access_token, reading_list_id):
+    print(access_token, reading_list_id)
 
 
 if __name__ == "__main__":
-    main()
+    access_token, db_id = read_auth()
+    main(access_token, db_id)
